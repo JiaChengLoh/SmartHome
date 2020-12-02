0fb0d89a7cc12f73d1c469cb430593974271965e
Licz728 <vincentli_73@hotmail.com>
2020-12-02 21:06:11 +0800

    add_button

--- app/src/main/java/my/edu/tarc/smarthome/ui/home/HomeFragment.kt
+++ app/src/main/java/my/edu/tarc/smarthome/ui/home/HomeFragment.kt
@@ -4,21 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.database.FirebaseDatabase
 import my.edu.tarc.smarthome.R
+
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
+    var database = FirebaseDatabase.getInstance()
+
+    //Getting Database Reference
+    var databaseReference = database.reference
+
+    //Getting Reference to Root Node
+    var myRef = database.getReference("PI_04_CONTROL/buzzer")
+
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
+
+        //Setting Value
+        myRef.setValue("1")
+
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
@@ -28,4 +43,6 @@ class HomeFragment : Fragment() {
         })
         return root
     }
+
+
 }
--- app/src/main/java/my/edu/tarc/smarthome/ui/home/HomeViewModel.kt
+++ app/src/main/java/my/edu/tarc/smarthome/ui/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package my.edu.tarc.smarthome.ui.home
 
+import android.widget.Button
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,4 +11,5 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
 }
