b01f5d654888d16602081ebebf9c613824f6c03a
Licz728 <vincentli_73@hotmail.com>
2020-12-04 21:55:04 +0800

    new 1

--- app/src/main/java/my/edu/tarc/smarthome/ui/bathroom/BathroomFragment.kt
+++ app/src/main/java/my/edu/tarc/smarthome/ui/bathroom/BathroomFragment.kt
@@ -1,25 +1,27 @@
 package my.edu.tarc.smarthome.ui.bathroom
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.fragment_bathroom.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 import my.edu.tarc.smarthome.R
 
-class BathroomFragment: Fragment() , View.OnClickListener{
+class BathroomFragment: Fragment() , SensorEventListener {
     private lateinit var bathroomViewModel: BathroomViewModal
 
-
     var database = FirebaseDatabase.getInstance()
-
-    //var ctx: Context? = null
 
     //Getting Database Reference
     var databaseReference = database.reference
@@ -29,23 +31,28 @@ class BathroomFragment: Fragment() , View.OnClickListener{
     var myLcdScr = database.getReference("PI_04_CONTROL/lcdscr")
     var myLcd = database.getReference("PI_04_CONTROL/lcdtxt")
     var myRelay = database.getReference("PI_04_CONTROL/relay1")
+    var myLed = database.getReference("PI_04_CONTROL/ledlgt")
+    var myBzr = database.getReference("PI_04_CONTROL/buzzer")
 
+    private lateinit var mSensorManager: SensorManager
+    private var mSensors: Sensor? = null
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         super.onCreate(savedInstanceState)
         val view = inflater.inflate(R.layout.fragment_bathroom, container, false)
         bathroomViewModel = ViewModelProvider(this).get(BathroomViewModal::class.java)
-        val textView: TextView = view.findViewById(R.id.text_bathroom)
-        val btn_on: Button = view.findViewById(R.id.btn_reminder_on)
-        //val btn_off: Button = view.findViewById(R.id.btn_panic_off)
-        //textViewPanic.setText("Panic Button")
-        //textViewPanic.setOnClickListener(this)
-        btn_on.setOnClickListener(this)
-        //btn_off.setOnClickListener(this)
+        mSensorManager = this.activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        //mSensorManager = view.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+//        Define the sensor
+        mSensors = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
 
         bathroomViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
@@ -53,34 +60,36 @@ class BathroomFragment: Fragment() , View.OnClickListener{
         return view
     }
 
-
-    override fun onClick(view: View?) {
-        when (view?.id) {
-            R.id.btn_reminder_on-> {
-                //Setting Value
-                bathroomOn()
-                Toast.makeText(getActivity(),"Light ON", Toast.LENGTH_SHORT).show();
-
-            }
-
-        }
-    }
-
-    private fun bathroomOn() {
+    private fun bathroomOff() {
         myLcdScr.setValue("1")
-        myLcd.setValue("===LIGHT===ON===")
-        myRelay.setValue("1")
-
+        myLcd.setValue("===BUZZER==OFF===")
+        myBzr.setValue("0")
     }
 
-
+    override fun onSensorChanged(p0: SensorEvent?) {
+//        Sensor change value
+        val millibarsOfPressure = p0!!.values[0]
+        if (p0.sensor.type == Sensor.TYPE_LIGHT)
+            Toast.makeText(this.activity, "" + millibarsOfPressure + " lx", Toast.LENGTH_SHORT).show()
+                if (millibarsOfPressure <= 10) {
+                    myLed.setValue("1")
+                    bathroom_image.setImageResource(R.drawable.open_turned_on_light)
+                    bathroom_text.text = "LED Light ON"
+                }
+                else {
+                    myLed.setValue("0")
+                    bathroom_image.setImageResource(R.drawable.open_turned_off_light)
+                    bathroom_text.text = "LED Light OFF"
+                }
+    }
 
     override fun onResume() {
         super.onResume()
-
+        mSensorManager.registerListener(this, mSensors, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
         super.onPause()
+        mSensorManager.unregisterListener(this)
     }
 }
